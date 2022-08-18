f9b72b7142b271da71a348468a2a210518ded23b
Reza Anugrah Prakasa <rezaprakasa@gmail.com>
2022-08-18 11:26:38 +0700

    Initializing Spring Project

---
+++ src/main/java/com/ist/servicemotorapi/spring/core/ServiceMotorApiApplication.java
@@ -0,0 +1,13 @@
+package com.ist.servicemotorapi.spring.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMotorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceMotorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ist/servicemotorapi/spring/core/ServiceMotorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ist.servicemotorapi.spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMotorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
